da3344101a5dbe16d27cca057075fed45f7da078
guilherme <guilherme@adsim.com.br>
2018-11-14 06:59:20 -0200

    Alteracao de classe de componente

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Foto from './Foto';
+import FotoItem from './Foto';
 
 export default class Timeline extends Component {
     render(){
         return (
         <div className="fotos container">
-          <Foto/>
-          <Foto/>
+          <FotoItem/>
+          <FotoItem/>
         </div>            
         );
     }
