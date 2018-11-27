cc2d8325fde3a001421ec4c5e21841bed7517332
guilherme <guilherme@adsim.com.br>
2018-11-27 08:56:17 -0200

    React Router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
 import App from './App';
+import Login from './componentes/Login';
+import {BrowserRouter,Switch, Route} from 'react-router-dom';
 
 
 ReactDOM.render(
-  <App />,
+ (<BrowserRouter>
+    <Switch>
+      <Route path="/" exact={true} component={Login}/>
+      <Route path="/timeline"  component={App}/>
+    </Switch>       
+ </BrowserRouter>),
   document.getElementById('root')
 );
+
